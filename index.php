d6477b29ee9ca30d76d42be164e01522863e10fe
Charlie <vmdx.doom@gmail.com>
2011-03-03 07:55:04 +0000

    updated the index page

--- index.php
+++ index.php
@@ -33,20 +33,19 @@
         <a href="bbcode_lookup.php"><img class="tool_img" src="media/bbcode_tool_spotlight.png" alt="bbcode_spotlight"></a>
         <a href="bbcode_lookup.php"><span class="tool_title" id="bbcode_tool">BBCode Translator</span></a>
         <span class="tool_desc">Converts backpack contents to eye-catching BBCode for trading on forums</span>
-        <span class="tool_news">v1.0 - updated 2.23.2011</span>
+        <span class="tool_news">updated 2.24.2011</span>
       </div>
-      
+      <div class="tool">
+        <a href="metal_lookup.php"><img class="tool_img" src="media/metal_tool_spotlight.png" alt="bbcode_spotlight"></a>
+        <a href="metal_lookup.php"><span class="tool_title" id="metal_tool">Metal Maker</span></a>
+        <span class="tool_desc">Organizes backpack weapons into scrap metal and token combinations</span>
+        <span class="tool_news">updated 3.2.2011</span>
+      </div>
 
     </div>
     
     <div id="indev_tools" class="toolbox">
       <span class="tools_title">Tools In Development</span>
-      <div class="tool">
-        <a href="metal_lookup.php"><img class="tool_img" src="media/metal_tool_spotlight.png" alt="bbcode_spotlight"></a>
-        <a href="metal_lookup.php"><span class="tool_title" id="metal_tool">Metal Maker</span></a>
-        <span class="tool_desc">Organizes backpack weapons into scrap metal and token combinations</span>
-        <span class="tool_news">v0.1 - updated 2.23.2011</span>
-      </div>
       <div class="tool">
         <img class="tool_img" src="media/dictionary_tool_spotlight.png" alt="bbcode_spotlight">
         <span class="tool_title" id="dict_tool">TF2 Item Dictionary</span>
