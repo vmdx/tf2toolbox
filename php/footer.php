f8a7770450bc92bd20711fbd585d50e45e7f6092
Charlie <vmdx.doom@gmail.com>
2011-09-05 04:40:10 +0000

    trashing php tf2toolbox. goodbye forever\!

--- php/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo '
-  <div id="footer">
-    <span id="footer_text"><a href="about.php">About</a> &#8226 <a href="changelog.txt">Changelog</a> &#8226 <a href="http://formspring.me/tf2toolbox" target="_blank">Give Feedback</a> &#8226 Created by <a href="http://lions.dongues.net" target="_blank">&gt;:3</a> <a href="http://steamcommunity.com/id/vmdx/" target="_blank">VMDX</a></span>
-  </div>
-';
-
-?>
