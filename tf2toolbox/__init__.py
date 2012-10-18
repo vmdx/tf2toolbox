e52c5a8d34c3461d142c740b6714ec0675913911
vmdx <vmdx.doom@gmail.com>
2012-10-17 20:44:05 -0700

    failover to config.py.example for testing. priority use of environment vars, with secure encrypted env var in travis yml

--- tf2toolbox/__init__.py
+++ tf2toolbox/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+import os.path
 app = Flask(__name__)
 
-app.config.from_pyfile('config.py')
+# if config.py does not exist, use config.py.example.
+if not os.path.exists('config.py'):
+    print '[WARNING] Using config.py.example!'
+    app.config.from_pyfile('config.py.example')
+else:
+    app.config.from_pyfile('config.py')
+
 app.jinja_env.trim_blocks = True
 
 import tf2toolbox.views
