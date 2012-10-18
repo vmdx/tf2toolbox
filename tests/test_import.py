ad8074ce18ca044b60ee4ac5a4afc675412e5682
vmdx <vmdx.doom@gmail.com>
2012-10-17 19:48:55 -0700

    initial set of tests - just import all modules. travis.yml hook

---
+++ tests/test_import.py
@@ -0,0 +1,31 @@
+"""
+Barebones test to check for syntax.
+"""
+
+def test_init():
+    import tf2toolbox
+
+def test_bptext():
+    import tf2toolbox.bptext
+
+def test_exceptions():
+    import tf2toolbox.exceptions
+
+def test_metadata():
+    import tf2toolbox.metadata
+
+def test_metal():
+    import tf2toolbox.metal
+
+def test_steamapi():
+    import tf2toolbox.steamapi
+
+def test_utils():
+    import tf2toolbox.utils
+
+def test_views():
+    import tf2toolbox.views
+
+def test_weapons():
+    import tf2toolbox.weapons
+
