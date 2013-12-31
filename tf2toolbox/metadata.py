40ade44c22d59169d69a3e79027dc71705617d8e
vmdx <vmdx.doom@gmail.com>
2013-12-30 16:00:17 -0800

    metadata.py - add new collector's quality

--- tf2toolbox/metadata.py
+++ tf2toolbox/metadata.py
@@ -225,7 +225,8 @@ QUALITY_BBCODE_MAP = {
     'Community': '[color=#70B04A]Community ',
     'Self-Made': '[color=#70B04A]Self-Made ',
     'Valve': '[color=#A50F79]Valve ',
-    'Haunted': '[color=#8650AC]Haunted '
+    'Haunted': '[color=#8650AC]Haunted ',
+    "Collector's": "[color=#830000]Collector's ",
 }
 
 # Weapon Stock data
